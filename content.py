0a38db8326ff531c8a8d0b682c5080dbb0de3932
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2025-01-14 09:18:46 -0500

    Update content.py

--- content.py
+++ content.py
@@ -1,9 +1,12 @@
+
+
+
 import simplegui
 
 def draw(canvas):
      canvas.draw_circle((100, 100), 60, 5, "Black", "Mediumvioletred")
-
-
+     canvas.draw_line((1, 500), (600, 500), 50, "forestgreen")
+     canvas.draw_line((1,600),(600,600),200, "forestgreen")
 
 
 
@@ -44,10 +47,6 @@ frame = simplegui.create_frame("My G4C Game", 600,600)
 frame.set_draw_handler(draw)
 frame.set_canvas_background("lightskyblue")
 
-
-
-
-frame.start()
 
 
 
