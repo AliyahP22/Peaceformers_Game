ec10a79fdff54cbbbfa00eece8b03c61b14d1fe5
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2025-01-16 08:55:16 -0500

    Update content.py

--- content.py
+++ content.py
@@ -5,7 +5,15 @@ def draw(canvas): #setting of game
      canvas.draw_line((1, 500), (600, 500), 50, "forestgreen")
      canvas.draw_line((1,600),(600,600),200, "forestgreen")
 #Villagers of game
-     canvas.draw_circle((45,300),25,5,"black", "peru")
+     canvas.draw_circle([150, 100], 30, 2, 'Black', 'white')
+     # Body (line)
+     canvas.draw_line([150, 130], [150, 250], 4, 'Black')
+     # Arms (lines) 
+     canvas.draw_line([150, 150], [100, 200], 4, 'Black') 
+     canvas.draw_line([150, 150], [200, 200], 4, 'Black') 
+     # Legs (lines) 
+     canvas.draw_line([150, 250], [100, 350], 4, 'Black') 
+     canvas.draw_line([150, 250], [200, 350], 4, 'Black')
 
 
 
@@ -47,3 +55,4 @@ frame.set_canvas_background("lightskyblue")
 
 
 frame.start()
+
